795e540de0ff9688cc770527f6ee3bd3987dfedb
mrn-dns <marincasdenis@yahoo.com>
2024-12-09 11:37:42 +0000

    Amended new endpoint to include query string

--- lambdas/getMovieAwards.ts
+++ lambdas/getMovieAwards.ts
@@ -2,7 +2,7 @@ import { APIGatewayProxyHandlerV2 } from "aws-lambda";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient, QueryCommand } from "@aws-sdk/lib-dynamodb";
 
-const ddbDocClient = createDDbDocClient();
+const ddbDocClient = createDocumentClient();
 
 export const handler: APIGatewayProxyHandlerV2 = async (event, context) => {
   try {
@@ -10,15 +10,24 @@ export const handler: APIGatewayProxyHandlerV2 = async (event, context) => {
 
     const parameters = event?.pathParameters;
     const awardBody = parameters?.awardBody;
-    const movieId = parameters?.movieId ? parseInt(parameters.movieId) : undefined;
+    const movieId = parameters?.movieId
+      ? parseInt(parameters.movieId)
+      : undefined;
 
     if (!awardBody || !movieId) {
       return {
-        statusCode: 400,
-        headers: { "content-type": "application/json" },
-        body: JSON.stringify({ message: "Missing award body or movie ID" }),
+        statusCode: 404,
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ Message: "Missing award body or movie ID" }),
       };
     }
+
+    const queryStringParams = event.queryStringParameters;
+    const minAwards = queryStringParams?.min
+      ? parseInt(queryStringParams.min)
+      : undefined;
 
     const commandOutput = await ddbDocClient.send(
       new QueryCommand({
@@ -34,34 +43,46 @@ export const handler: APIGatewayProxyHandlerV2 = async (event, context) => {
     if (!commandOutput.Items || commandOutput.Items.length === 0) {
       return {
         statusCode: 404,
-        headers: { "content-type": "application/json" },
-        body: JSON.stringify({ message: "No awards found for the specified movie and award body" }),
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ Message: "No awards found" }),
+      };
+    }
+
+    const awardDetails = commandOutput.Items[0];
+    const numAwards = awardDetails.numAwards;
+
+    if (minAwards !== undefined && numAwards <= minAwards) {
+      return {
+        statusCode: 400,
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ Message: "Request failed" }),
       };
     }
 
     return {
       statusCode: 200,
-      headers: { "content-type": "application/json" },
-      body: JSON.stringify({ data: commandOutput.Items }),
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ data: awardDetails }),
     };
   } catch (error: any) {
     console.log(JSON.stringify(error));
     return {
       statusCode: 500,
-      headers: { "content-type": "application/json" },
+      headers: {
+        "content-type": "application/json",
+      },
       body: JSON.stringify({ error }),
     };
   }
 };
 
-function createDDbDocClient() {
+function createDocumentClient() {
   const ddbClient = new DynamoDBClient({ region: process.env.REGION });
-  const marshallOptions = {
-    convertEmptyValues: true,
-    removeUndefinedValues: true,
-    convertClassInstanceToMap: true,
-  };
-  const unmarshallOptions = { wrapNumbers: false };
-  const translateConfig = { marshallOptions, unmarshallOptions };
-  return DynamoDBDocumentClient.from(ddbClient, translateConfig);
+  return DynamoDBDocumentClient.from(ddbClient);
 }
