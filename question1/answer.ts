af4bdba8eaaca6c5590a53c0177b70ea42fa23e2
mrn-dns <marincasdenis@yahoo.com>
2024-12-09 10:37:00 +0000

    Function to search for matching transporters

--- question1/answer.ts
+++ question1/answer.ts
@@ -1,1 +1,52 @@
-console.log('Hello world')
+interface Car {
+    make: string;
+    color: string;
+    registration: string;
+    owner: string;
+  }
+  
+  interface Bicycle {
+    make: string;
+    color: string;
+    owner: string;
+  }
+  
+  type Transporter = Car | Bicycle;
+  
+  const database: Transporter[] = [
+   {   // Car
+      make: "Toyota Yaris",
+      color: "Red",
+      registration: "231WD1234",
+      owner: "Jane Smith",
+   },
+   {    // Car
+      make: "Suzuki Swift",
+      color: "Blue",
+      registration: "241WD4321",
+      owner: "Paul O Regan",
+   },
+   {   // Car
+      make: "Ford Puma",
+      color: "Blue",
+      registration: "241WD1212",
+      owner: "Eileen Silk",
+   },
+   {   // Bicycle
+      make: "Revel Rascal XO",
+      color: "Blue",
+      owner: "Cindy Tamoka",
+   },
+   {    // Bicycle
+      make: "Yeti SB140 LR",
+      color: "Red",
+      owner: " ",
+   },
+  ];
+
+  function getMatches(criteria: (transporter: Transporter) => boolean): Transporter[] {
+    return database.filter(criteria);
+  }
+
+  const blueTransporters = getMatches((t) => t.color === "Blue");
+  console.log("Blue Transporters:", blueTransporters);
